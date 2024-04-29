661cf38fb74749eaea68661fc3c619c763047e15
joechea <s.chea@aupp.edu.kh>
2024-04-29 09:30:04 +0700

    add config ignore file in nvim

--- init.lua
+++ init.lua
@@ -343,11 +343,12 @@ require('lazy').setup {
         -- You can put your default mappings / updates / etc. in here
         --  All the info you're looking for is in `:help telescope.setup()`
         --
-        -- defaults = {
-        --   mappings = {
-        --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-        --   },
-        -- },
+        defaults = {
+          -- mappings = {
+          --   i = { ['<c-enter>'] = 'to_fuzzy_refine' },
+          -- },
+          file_ignore_patterns = { 'node_modules', '.git', '.vscode', '.cache' },
+        },
         -- pickers = {}
         extensions = {
           ['ui-select'] = {
