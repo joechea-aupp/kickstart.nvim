1a845ec1857c68a441efb3cfd2b58814b6186698
joechea <s.chea@aupp.edu.kh>
2024-05-03 13:43:40 +0700

    update keymap using nvim v7 with desc key

--- init.lua
+++ init.lua
@@ -265,7 +265,7 @@ require('lazy').setup {
           else
             gitsigns.nav_hunk 'next'
           end
-        end)
+        end, { desc = 'Next Hunk' })
 
         map('n', '[c', function()
           if vim.wo.diff then
@@ -273,30 +273,30 @@ require('lazy').setup {
           else
             gitsigns.nav_hunk 'prev'
           end
-        end)
+        end, { desc = 'Previous Hunk' })
 
         -- Actions
-        map('n', '<leader>hs', gitsigns.stage_hunk)
-        map('n', '<leader>hr', gitsigns.reset_hunk)
+        map('n', '<leader>hs', gitsigns.stage_hunk, { desc = '[S]tage Hunk' })
+        map('n', '<leader>hr', gitsigns.reset_hunk, { desc = '[R]eset Hunk' })
         map('v', '<leader>hs', function()
           gitsigns.stage_hunk { vim.fn.line '.', vim.fn.line 'v' }
-        end)
+        end, { desc = '[S]tage Hunk' })
         map('v', '<leader>hr', function()
           gitsigns.reset_hunk { vim.fn.line '.', vim.fn.line 'v' }
-        end)
-        map('n', '<leader>hS', gitsigns.stage_buffer)
-        map('n', '<leader>hu', gitsigns.undo_stage_hunk)
-        map('n', '<leader>hR', gitsigns.reset_buffer)
-        map('n', '<leader>hp', gitsigns.preview_hunk)
+        end, { desc = '[R]eset Hunk' })
+        map('n', '<leader>hS', gitsigns.stage_buffer, { desc = '[S]tage Buffer' })
+        map('n', '<leader>hu', gitsigns.undo_stage_hunk, { desc = '[U]ndo Stage Hunk' })
+        map('n', '<leader>hR', gitsigns.reset_buffer, { desc = '[R]eset Buffer' })
+        map('n', '<leader>hp', gitsigns.preview_hunk, { desc = '[P]review Hunk' })
         map('n', '<leader>hb', function()
           gitsigns.blame_line { full = true }
-        end)
-        map('n', '<leader>tb', gitsigns.toggle_current_line_blame)
-        map('n', '<leader>hd', gitsigns.diffthis)
+        end, { desc = '[B]lame' })
+        map('n', '<leader>tb', gitsigns.toggle_current_line_blame, { desc = '[T]oggle [B]lame' })
+        map('n', '<leader>hd', gitsigns.diffthis, { desc = 'Diff [D]iff' })
         map('n', '<leader>hD', function()
           gitsigns.diffthis '~'
-        end)
-        map('n', '<leader>td', gitsigns.toggle_deleted)
+        end, { desc = 'Diff [D]elete' })
+        map('n', '<leader>td', gitsigns.toggle_deleted, { desc = 'Toggle [D]eleted' })
 
         -- Text object
         map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
