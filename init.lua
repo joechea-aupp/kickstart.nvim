1adc84402244be47105428c863b368fa7d161617
joechea <s.chea@aupp.edu.kh>
2024-05-03 15:03:57 +0700

    switch to github theme

--- init.lua
+++ init.lua
@@ -797,15 +797,15 @@ require('lazy').setup {
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`
     -- 'folke/tokyonight.nvim',
-    -- 'projekt0n/github-nvim-theme',
-    'aktersnurra/no-clown-fiesta.nvim',
+    'projekt0n/github-nvim-theme',
+    -- 'aktersnurra/no-clown-fiesta.nvim',
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
       -- Load the colorscheme here
       -- vim.cmd.colorscheme 'tokyonight-night'
-      -- vim.cmd.colorscheme 'github_dark_default'
-      vim.cmd.colorscheme 'no-clown-fiesta'
+      vim.cmd.colorscheme 'github_dark_default'
+      -- vim.cmd.colorscheme 'no-clown-fiesta'
 
       -- You can configure highlights by doing something like
       -- vim.cmd.hi 'Comment gui=none'
