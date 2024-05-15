26198743ead17c07778205d5dff4b14d62fee223
joechea <s.chea@aupp.edu.kh>
2024-05-15 14:38:25 +0700

    update tokyo night theme

--- init.lua
+++ init.lua
@@ -800,16 +800,26 @@ require('lazy').setup {
     -- change the command in the config to whatever the name of that colorscheme is
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`
-    -- 'folke/tokyonight.nvim',
-    'projekt0n/github-nvim-theme',
+    'folke/tokyonight.nvim',
+    -- 'projekt0n/github-nvim-theme',
     -- 'nanotech/jellybeans.vim',
     -- 'aktersnurra/no-clown-fiesta.nvim',
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
       -- Load the colorscheme here
-      -- vim.cmd.colorscheme 'tokyonight-night'
-      vim.cmd.colorscheme 'github_dark_default'
+      -- add theme configuration for tokyonight
+      require('tokyonight').setup {
+        style = 'storm',
+        transparent = true,
+        terminal_colors = true,
+        styles = {
+          sidebars = 'dark',
+          floats = 'dark',
+        },
+      }
+      vim.cmd.colorscheme 'tokyonight-night'
+      -- vim.cmd.colorscheme 'github_dark_default'
       -- vim.cmd.colorscheme 'no-clown-fiesta'
       -- vim.cmd.colorscheme 'jellybeans'
 
